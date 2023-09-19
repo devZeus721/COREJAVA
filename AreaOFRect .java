30801fd00f289ac5b6e77b63d27180ced57254fa
dev Zeus MAthew <devzeus.mathew721@gmail.com>
2023-09-19 22:53:53 +0530

    summitted
    final rechanged

---
+++ AreaOFRect .java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class AreaOFRect {
+
+    public static void main(String args[]) {
+
+        // Write code here
+
+        Scanner sc=new Scanner(System.in);
+
+        int length=sc.nextInt();
+
+        int breadth=sc.nextInt();
+
+        int area= (length*breadth);
+
+        System.out.println(area);   
+
+    }
+
+}
