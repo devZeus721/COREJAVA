790eb051b69a1f0e876787df285c415b19866603
devZeus MAthew <devzeus.mathew721@gmail.com>
2023-10-08 00:15:51 +0530

    Merge branch 'main' of https://github.com/devZeus721/COREJAVA

---
+++ booleanowneg.java
@@ -0,0 +1,13 @@
+public class booleanowneg {
+    public static void main(String[] args) {
+        boolean b = false;
+        b = true;
+        /*boolean toBe = false;
+        b = toBe || !toBe;
+        if (b) System.out.println(toBe);*/
+        boolean d = true;
+        boolean c = false;
+        boolean a = (d) && (c);            //The following line will give an error
+        System.out.println(a);
+    }
+}
