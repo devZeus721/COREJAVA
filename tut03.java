f9dd1e537848441f829ecd909d981cf8e75392bb
devZeus MAthew <devzeus.mathew721@gmail.com>
2023-10-15 13:42:36 +0530

    DOne
    Commited

---
+++ tut03.java
@@ -0,0 +1,9 @@
+//Loops
+
+//There are two kind of loops in Java, for and while.
+
+public class tut03 {
+    public static void main(String[] args) {
+
+    }
+}
--- tut3.java
+++
@@ -1,5 +0,0 @@
-public class tut3 {
-    public static void main(String[] args) {
-
-    }
-}
