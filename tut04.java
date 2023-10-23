dfe91e38f20354c251f67a3be119dbcf1a8b6423
devZeus MAthew <devzeus.mathew721@gmail.com>
2023-10-23 19:30:55 +0530

    DOne
    Commited

--- tut04.java
+++ tut04.java
@@ -53,4 +53,4 @@ public class tut04 {
             bar2(joe, jack);
             }
         }
-}
+
---
+++ tut04b.java
@@ -0,0 +1,27 @@
+//Functions b
+/*
+Non-static methods
+Non-static methods in Java are used more than static methods.
+Those methods can only be run on objects and not on the whole class.
+Non-static methods can access and alter the field of the object.
+public class Student {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+* */
+public class tut04b {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+        }
+
