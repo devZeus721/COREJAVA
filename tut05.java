0ce0c81624e1592daf81278f126160f59ad1d603
devZeus MAthew <devzeus.mathew721@gmail.com>
2023-10-24 21:12:26 +0530

    DOne
    Commited

---
+++ tut05.java
@@ -0,0 +1,7 @@
+//Objects
+
+/* Everything in Java is within classes and objects.
+    Java objects hold a state, state are variables which are saved together within an object,
+    we call them fields or member variables.*/
+public class tut05 {
+}
