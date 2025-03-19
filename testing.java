10942e567f9a1b4f8f6f76d5a704b3d5e30d6b06
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-03-19 06:13:11 +0530

    completed recovery
    comitted

---
+++ testing.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class testing {
+    public static void main(String[] args) {
+        // Create a JFrame (main window)
+        JFrame frame = new JFrame("Swing Example");
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create a JButton
+        JButton button = new JButton("Click MeOperation Success");
+        
+
+        // Add the button to the frame
+        frame.add(button);
+        
+
+        // Make the frame visible
+        frame.setVisible(true);
+    }
+}
